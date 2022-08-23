c9a77b874770fee3d8f67bf869f6af86f8200b2e
Evgenii Vaniukov <evgeniivaniukov64@gmail.com>
2022-08-23 21:50:35 +0300

    Commit

---
+++ Arithmetic/new/target/debug/deps/new.d
@@ -0,0 +1,5 @@
+C:\Users\evgen\OneDrive\Документы\GitHub\Rust\Arithmetic\new\target\debug\deps\new.exe: src\main.rs
+
+C:\Users\evgen\OneDrive\Документы\GitHub\Rust\Arithmetic\new\target\debug\deps\new.d: src\main.rs
+
+src\main.rs:
---
+++ Arithmetic/new/target/debug/new.d
@@ -0,0 +1,1 @@
+C:\Users\evgen\OneDrive\Документы\GitHub\Rust\Arithmetic\new\target\debug\new.exe: C:\Users\evgen\OneDrive\Документы\GitHub\Rust\Arithmetic\new\src\main.rs
